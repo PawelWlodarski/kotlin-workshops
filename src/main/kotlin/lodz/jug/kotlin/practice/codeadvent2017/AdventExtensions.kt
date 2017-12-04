41d87a06bf2ccaec21a0e1a638e3eb7c8a5b07d9
pawel <pawel@domena.pl>
2017-12-04 20:07:50 +0100

    adventofcode day 4 - part 2

--- src/main/kotlin/lodz/jug/kotlin/practice/codeadvent2017/AdventExtensions.kt
+++ src/main/kotlin/lodz/jug/kotlin/practice/codeadvent2017/AdventExtensions.kt
@@ -4,4 +4,5 @@ object AdventExtensions{
     fun Char.asDigit() = Integer.parseInt(this.toString())
     fun String.splitWhitespace() = this.split("\\s+".toRegex())
     fun String.lines()= this.split("\n")
+    fun Int.pow2():Int = Math.pow(this.toDouble(),2.0).toInt()
 }
--- src/main/kotlin/lodz/jug/kotlin/practice/codeadvent2017/Day4.kt
+++ src/main/kotlin/lodz/jug/kotlin/practice/codeadvent2017/Day4.kt
@@ -3,7 +3,7 @@ package lodz.jug.kotlin.practice.codeadvent2017
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.WordSpec
 import lodz.jug.kotlin.practice.codeadvent2017.AdventExtensions.splitWhitespace
-import lodz.jug.kotlin.practice.codeadvent2017.AdventExtensions.lines
+import lodz.jug.kotlin.practice.codeadvent2017.AdventExtensions.pow2
 import java.io.File
 
 private fun checkPhrase(input: String): Boolean {
@@ -11,8 +11,19 @@ private fun checkPhrase(input: String): Boolean {
     return words.size == words.toSet().size
 }
 
+private fun checkAnagramPhrase(input:String) : Boolean {
+    fun encodeWord(s:String):Int = s.map { it.toInt().pow2() }.sum()
+    val words=input.splitWhitespace()
+    return words
+            .map (::encodeWord)
+            .distinct()
+            .size == words.size
+}
 
-private fun countPhrases(lines: List<String>) = lines.filter(::checkPhrase).count()
+
+private fun countPhrases(phraseChecker:(String)->Boolean) : (List<String>) -> Int ={lines ->
+    lines.filter(phraseChecker).count()
+}
 
 class PhraseTest : WordSpec() {
     init {
@@ -22,9 +33,22 @@ class PhraseTest : WordSpec() {
             checkPhrase("aa bb cc dd aaa") shouldBe true
         }
 
+        "test anagrams" {
+            checkAnagramPhrase("abcde fghij") shouldBe true
+            checkAnagramPhrase("abcde xyz ecdab") shouldBe false
+            checkAnagramPhrase("a ab abc abd abf abj") shouldBe true
+            checkAnagramPhrase("iiii oiii ooii oooi oooo") shouldBe true
+            checkAnagramPhrase("oiii ioii iioi iiio") shouldBe false
+        }
+
         "phrases part 1" {
             val input = File("files/day4.txt").readLines()
-            println(countPhrases(input))
+            println(countPhrases(::checkPhrase)(input))
+        }
+
+        "phrases part 3" {
+            val input = File("files/day4.txt").readLines()
+            println(countPhrases(::checkAnagramPhrase)(input))
         }
     }
 
