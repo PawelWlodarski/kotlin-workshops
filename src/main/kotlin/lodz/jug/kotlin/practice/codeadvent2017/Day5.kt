394acfdcbdaf946edf84e69d4dee1abad6b7f04f
pawel <pawel@domena.pl>
2017-12-07 15:04:57 +0100

    adventofcode day 5 - part 2

--- src/main/kotlin/lodz/jug/kotlin/practice/codeadvent2017/Day5.kt
+++ src/main/kotlin/lodz/jug/kotlin/practice/codeadvent2017/Day5.kt
@@ -14,10 +14,10 @@ private fun parseSteps(): IntArray =
 
 
 
-private fun runProcedure(procedure: IntArray): Int {
+private fun runProcedure(updateStrategy:(Int) -> Int): (IntArray) -> Int = {procedure ->
     fun nextPosition(current: Int) = (current + procedure[current])
     fun updatePosition(position: Int) {
-        procedure[position] = procedure[position] + 1
+        procedure[position] = updateStrategy(procedure[position])
     }
 
     tailrec fun procedureStep(position: Int, steps: Int): Int = when {
@@ -30,18 +30,32 @@ private fun runProcedure(procedure: IntArray): Int {
 
     }
 
-    return procedureStep(0, 1)
+    procedureStep(0, 1)
 }
+
+private val incrementalUpdate = { i:Int -> i+1}
+private val stabilizingUpdate: (Int) -> Int= {if (it>=3) it-1 else it+1}
 
 class TestJumpProcedure : WordSpec() {
     init {
-        "should properly perform procedure"{
+        "should properly perform procedure for step1"{
             val input = intArrayOf(0, 3, 0, 1, -3)
-            runProcedure(input) shouldBe 5
+            runProcedure(incrementalUpdate)(input) shouldBe 5
+        }
+
+        "should properly perform procedure for step 2"{
+            val input = intArrayOf(0, 3, 0, 1, -3)
+            runProcedure(stabilizingUpdate)(input) shouldBe 10
         }
 
         "part 1" {
-            val program=::parseSteps andThen ::runProcedure
+            val program=::parseSteps andThen runProcedure(incrementalUpdate)
+
+            println(program())
+        }
+
+        "part 2" {
+            val program=::parseSteps andThen runProcedure(stabilizingUpdate)
 
             println(program())
         }
