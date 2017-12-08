771839f466d98ee0c86f2435bb30cf745429192d
PawelWlodarski <p.wlodarski@oberthur.com>
2017-12-08 11:42:24 +0100

    day7 part 1

--- src/main/kotlin/lodz/jug/kotlin/practice/codeadvent2017/AdventExtensions.kt
+++ src/main/kotlin/lodz/jug/kotlin/practice/codeadvent2017/AdventExtensions.kt
@@ -1,11 +1,14 @@
 package lodz.jug.kotlin.practice.codeadvent2017
 
+import java.io.File
 import java.util.*
 
 object AdventExtensions{
     fun Char.asDigit() = Integer.parseInt(this.toString())
     fun String.splitWhitespace() = this.split("\\s+".toRegex())
     fun String.lines()= this.split("\n")
+    fun String.readLines()= File(this).readLines()
+
     fun Int.pow2():Int = Math.pow(this.toDouble(),2.0).toInt()
 
     fun IntArray.indexOfMax()= this.indexOf(this.max()!!)
