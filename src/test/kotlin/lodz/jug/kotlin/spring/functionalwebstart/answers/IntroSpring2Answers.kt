0017f11536019d25d7f61573476241f6b95d966b
PawelWlodarski <p.wlodarski@oberthur.com>
2019-02-13 16:12:41 +0100

    Building DSLs Exercises

---
+++ src/test/kotlin/lodz/jug/kotlin/spring/functionalwebstart/answers/IntroSpring2Answers.kt
@@ -0,0 +1,113 @@
+package lodz.jug.kotlin.spring.functionalwebstart.answers
+
+import arrow.core.andThen
+import arrow.core.identity
+import org.amshove.kluent.`should contain all`
+import org.amshove.kluent.shouldBe
+import org.junit.Test
+
+typealias Pipe = (String) -> String
+
+class IntroSpring2Answers{
+
+
+    @Test
+    fun buildIndexedNews(){
+
+        val trim:(String)->String=String::trim
+        val toLower : Pipe = String::toLowerCase
+        val processor : Pipe = toLower andThen trim andThen String::capitalize
+
+        val result=simpleNews{
+            article("world".tag, "world news",String::capitalize)
+            article("domestic".tag, "     Domestic news",String::trim)
+            article("domestic".tag, "   domestic local news",trim andThen String::capitalize)
+            article("fun".tag, "   funny news",{s:String -> s.trim()} andThen String::capitalize)
+            article("fun".tag, "    more FUN",processor)
+            article("fun".tag, "It's a prank bro!",::identity)
+        }
+
+
+        result.getValue(Tag("world")) `should contain all`   arrayListOf("World news")
+        result.getValue(Tag("domestic")) `should contain all`   arrayListOf("Domestic news","Domestic local news")
+        result.getValue(Tag("fun")) `should contain all`   arrayListOf("Funny news","More fun","It's a prank bro!")
+    }
+
+
+    @Test
+    fun buildMultiNews(){
+          val articles = multiNews {
+              article(Tag2("world"),"world article 1")
+              article("world".or("fun"),"funny things on the world")
+              article("world" or "fun" or "sport","funny world cup")
+              article(Tag2("fun"),"just fun")
+          }
+
+
+        articles.getFirstMatching("world") shouldBe "world article 1"
+        articles.getFirstMatching("fun") shouldBe "funny things on the world"
+        articles.getFirstMatching("sport") shouldBe "funny world cup"
+    }
+}
+
+
+fun simpleNews(operation : SimpleNewsDsl.() -> Unit) = SimpleNewsDsl().apply(operation).display()
+data class Tag(val v:String)
+
+class SimpleNewsDsl{
+
+    private val articles = mutableMapOf<Tag,List<String>>()
+
+    val String.tag:Tag
+        get() = Tag(this)
+
+    fun article(tag:Tag,content : String,processor : (String) -> String){
+        articles[tag] = articles[tag].orEmpty() + processor(content)
+    }
+
+
+    fun display(): Map<Tag, List<String>> = articles
+
+}
+
+
+fun multiNews(operation : MultiNewsDsl.() -> Unit):MultiNewsDsl.Articles = MultiNewsDsl().apply(operation).allArticles()
+
+sealed class Tag2{
+    abstract fun matches(other : String) :  Boolean
+    companion object {
+        operator fun invoke(tag:String) = SingleTag(tag)
+    }
+}
+
+class SingleTag(private val tag:String) : Tag2() {
+    override fun matches(other: String): Boolean  = tag == other
+}
+
+class MultiTag(private val tags:Set<String>) : Tag2(){
+    fun append(other:String) = MultiTag(tags + other)
+    override fun matches(other: String): Boolean  = tags.contains(other)
+}
+
+class MultiNewsDsl {
+    private var articles =  listOf<Pair<Tag2,String>>()
+
+    companion object {
+        const val NOT_FOUND : String = "NOT_FOUND"
+    }
+
+    infix fun String.or(other:String) = MultiTag(setOf(this,other))
+    infix fun MultiTag.or(other: String) = this.append(other)  //why this can not access to a private field ???
+
+    fun article(tag:Tag2,content : String){
+            articles = articles + (tag to content)
+    }
+
+    fun allArticles() : Articles = Articles(articles)
+
+    class Articles(private val articles: List<Pair<Tag2, String>>){
+        fun getFirstMatching(tag:String): String = articles.find { (tag2,_)-> tag2.matches(tag)} ?.second ?: NOT_FOUND
+    }
+
+}
+
