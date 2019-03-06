5550ad2af2e60de48d8b4d207093cdab1eb18b76
PawelWlodarski <p.wlodarski@oberthur.com>
2019-03-06 13:25:25 +0100

    Exercise 4

---
+++ src/test/kotlin/lodz/jug/kotlin/spring/functionalwebstart/exercises/IntroSpring4Exercises.kt
@@ -0,0 +1,115 @@
+package lodz.jug.kotlin.spring.functionalwebstart.exercises
+
+import arrow.core.*
+import io.kotlintest.fail
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldBeInstanceOf
+import org.junit.Test
+
+class IntroSpring4Exercises {
+
+    class Bean1(val info:String="Bean1"){  //WHAT HAPPEN WITHOUT DEFAULT VALUE ?
+        override fun toString(): String {
+            return "Bean1(info='$info')"
+        }
+    }
+    class Bean2(private val info:String="Bean2"){
+        override fun toString(): String {
+            return "Bean2(info='$info')"
+        }
+    }
+    class Bean3(private val b1:Bean1, private val b2:Bean2){
+        override fun toString(): String {
+            return "Bean3(b1=$b1, b2=$b2)"
+        }
+    }
+
+    @Test
+    fun shouldAddNamedBeansToConfig(){
+            val ctx = NamedContext()
+            ctx.register<Bean1>()
+            ctx.register<Bean1>()
+            ctx.register<Bean1>("namedBean1")
+
+            ctx.register<Bean2>()
+            ctx.register<Bean2>("namedBean2")
+
+            ctx.register("b3"){
+                val b1=findNamed<Bean1>("namedBean1")
+                val b2 =findNamed<Bean2>("namedBean2")
+
+                val b3: Bean3? =b1?.let { bean1 ->
+                    b2?.let { bean2 ->
+                        Bean3(bean1,bean2)
+                    }
+                }
+
+                b3 ?: throw RuntimeException("incorrect configuration")
+            }
+
+            val foundB3=ctx.findNamed<Bean3>("b3")
+            println(foundB3)
+
+    }
+
+    @Test
+    fun shouldUseOptionalAPI(){
+        val ctx = NamedContext()
+        ctx.findOptional<Bean1>() shouldBe None
+
+        ctx.register<Bean1>()
+        ctx.register<Bean1>()
+        ctx.register<Bean2>()
+        when(val candidate1=ctx.findOptional<Bean1>()){
+            is None -> fail("bean1 should be some")
+            is Some -> candidate1.t.info shouldBe "Bean1"
+        }
+
+
+        ctx.register{
+            val b1Candidate: Option<Bean1> =findOptional()  //NO Generics here!!
+            val b2Candidate: Option<Bean2> =findOptional()
+
+            val b3candidate: Option<Bean3> =b1Candidate.flatMap { b1 ->   // EXPLAIN FOR-COMPREHENSION IDEA
+                b2Candidate.map {b2 ->
+                    Bean3(b1,b2)
+                }
+            }
+
+            b3candidate.getOrElse { throw java.lang.RuntimeException("wrong configuration") }
+        }
+
+
+        ctx.findOptional<Bean3>() shouldBeInstanceOf Some::class.java
+    }
+
+
+    class NamedContext{
+        data class BeanContext<T>(val name:String,val instance:T)
+
+
+        private var beans:List<BeanContext<*>> = emptyList()
+
+        inline fun <reified T:Any> register(name:String? = null){
+            TODO()
+        }
+
+        fun <T : Any> register(c:Class<T>,name:String? = null) {
+            TODO()
+        }
+
+        fun <T : Any> register(name:String? = null, init : NamedContext.() -> T){
+            TODO()
+        }
+
+
+        @Suppress("UNCHECKED_CAST")
+        fun <T : Any> findNamed(name:String):T? = TODO()
+
+        inline fun <reified T : Any> findOptional():Option<T> = TODO()
+
+        fun <T : Any> findOptional(c:Class<T>):Option<T> = TODO()
+    }
+
+
+}
